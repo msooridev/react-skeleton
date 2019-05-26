212c1eebb7fd75a80c2fd40e5d19a9e65f4afc9d
msoori <mahesh_soori@yahoo.com>
2019-05-26 00:18:34 -0400

    initial commit

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,17 +1,17 @@
-
 var React = require('react');
-var ListItem = require('./ListItem');
+var ListItem = require('./ListItem.jsx');
 
 var ingrediants = [
-                        {"id": 1, "text": "ham"},
-                        {"id": 2, "text": "cheese"},
-                        {"id": 3, "text": "potatoes"}
-                    ];
+    {"id": 1, "text": "ham"},
+    {"id": 2, "text": "cheese"},
+    {"id": 3, "text": "eggs"},
+    {"id": 4, "text": "potatoes"}
+];
 
 var List = React.createClass({
     render: function () {
         var listItems = ingrediants.map(function (item) {
-            return <ListItem key={item.id} ingredient={item.text}/>
+            return <ListItem key={item.id} ingrediant={item.text}/>
         })
 
         return (<ul>{listItems}</ul>);
